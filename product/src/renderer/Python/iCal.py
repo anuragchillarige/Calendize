e855839952a7d1d4d4fbdc0c8397feb88f58c00d
anuragchillarige <pinganurag@gmail.com>
2022-08-08 09:56:15 -0700

    worked on the ical portion of the website

--- product/src/renderer/Python/iCal.py
+++ product/src/renderer/Python/iCal.py
@@ -18,48 +18,51 @@ def addToDB(url, docID):
     ending = datetime.now() + timedelta(days=10)
     end = (ending.year, ending.month, ending.day)
 
-    cal = icalendar.Calendar.from_ical(requests.get(url).text)
-    events = recurring_ical_events.of(cal).between(start, end)
+    try:
+        cal = icalendar.Calendar.from_ical(requests.get(url).text)
+        events = recurring_ical_events.of(cal).between(start, end)
 
-    for event in events:
-        name = str(event['SUMMARY'])
-        description = str(event['DESCRIPTION'])
-        if (description.__contains__('To see detailed information for automatically created events like this one, use the official Google Calendar app.')):
-            description = ""
-        start = event['DTSTART'].dt
-        start_time = start.strftime("%H:%M")
-        duration = str(event['DTEND'].dt - start)
+        for event in events:
+            name = str(event['SUMMARY'])
+            description = str(event['DESCRIPTION'])
+            if (description.__contains__('To see detailed information for automatically created events like this one, use the official Google Calendar app.')):
+                description = ""
+            start = event['DTSTART'].dt
+            start_time = start.strftime("%H:%M")
+            duration = str(event['DTEND'].dt - start)
 
-        day = event['DTSTART'].dt
-        end = event['DTEND'].dt
+            day = event['DTSTART'].dt
+            end = event['DTEND'].dt
 
-        data = {
-            "day": day,
-            "duration": {
-                "hours": duration[0: duration.index(":")],
-                "mins": duration[duration.index(":") + 1: duration.rindex(":")]
-            },
-            "start_time": start_time,
-            "name": name,
-            "details": description,
-            "end": end
-        }
+            data = {
+                "day": day,
+                "duration": {
+                    "hours": duration[0: duration.index(":")],
+                    "mins": duration[duration.index(":") + 1: duration.rindex(":")]
+                },
+                "start_time": start_time,
+                "name": name,
+                "details": description,
+                "end": end
+            }
 
-        db.collection("users").document(docID).collection("events").document(f'iCal {name} ON {start} AT {start_time}').set(data)
+            db.collection("users").document(docID).collection("events").document(f'iCal {name} ON {start} AT {start_time}').set(data)
+            return True
+    except:
+        return False;
 
 def addCalendars(docID):
-    # urls = ['https://calendar.google.com/calendar/ical/pinganurag%40gmail.com/private-8d6688faf4d85816c9e261705bc60fdf/basic.ics']
-    urls = []
-    print(docID)
 
     docRef = db.collection("users").document(docID)
 
     doc = docRef.get()
 
     if (doc.exists):
-        link = doc.to_dict()['iCalLink']
-        print(link)
-        addToDB(link, docID)
+        links = doc.to_dict()['iCalLinks']
+        for link in links:
+            added = addToDB(link, docID)
+            if (added == False):
+                return "ERROR!"
     else:
         return "ERROR!"
 
--- website/src/Pages/Calendar.jsx
+++ website/src/Pages/Calendar.jsx
@@ -2,17 +2,24 @@ import { useRef } from "react";
 import Navbar from "../Components/Navbar";
 import '../Styles/Calendar.css'
 import { db, auth } from "../firebase";
-import { query, collection, getDocs, where, updateDoc, doc } from 'firebase/firestore';
+import { query, collection, getDocs, where, updateDoc, doc, onSnapshot } from 'firebase/firestore';
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Calendar() {
 
     const link = useRef();
+    const [links, setLinks] = useState([])
     const [currUser, loading] = useAuthState(auth);
-    let userDoc;
+    const [user, setUser] = useState("")
 
-    const getUser = async () => {
+    const submit = async () => {
+        if (loading)
+            return;
+
+        if (link.current.value === '')
+            return;
+
         if (loading)
             return;
         try {
@@ -21,32 +28,75 @@ export default function Calendar() {
                 where('uid', '==', currUser?.uid)
             );
             const theDoc = await getDocs(q);
-            await updateDoc(doc(db, 'users', theDoc.docs[0].id), { iCalLink: link.current.value })
+            setUser(theDoc.docs[0].id)
+            let newLinks = [...links, link.current.value]
+            if (newLinks.length <= 5) {
+                await updateDoc(doc(db, 'users', theDoc.docs[0].id), { iCalLinks: newLinks })
+                setLinks(newLinks)
+            } else {
+                alert("Only 5 iCal links allowed.")
+            }
+            link.current.value = ''
 
         } catch (e) {
-            // alert("Error: Could not get user doc.")
             console.log(e)
         }
     }
 
-    const submit = async () => {
-        if (loading)
-            return;
-        console.log(userDoc)
-    }
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const q = query(
+                    collection(db, 'users'),
+                    where('uid', '==', currUser?.uid)
+                );
+                const userDoc = await getDocs(q);
+                // return userDoc.docs[0].id;
+                await setUser(userDoc.docs[0].id)
+
+
+            } catch (err) {
+                return;
+            }
+        }
+
+        const func = async () => {
+            await getUser();
+            if (user !== '') {
+                const q = query(collection(db, "users"), where('uid', '==', currUser?.uid));
+                const userDoc = await getDocs(q)
+
+                const unsub = onSnapshot(doc(db, "users", userDoc.docs[0].id), (doc) => {
+                    setLinks(doc.data().iCalLinks)
+                })
+            }
+        }
+
+        func();
+
+    }, [currUser, user])
 
     return (
-        <>
+        <div>
             <Navbar title="Calendar" />
             <div className="calendar-holder">
                 <h1>Import Calendar</h1>
-                <form onSubmit={(e) => { e.preventDefault(); getUser() }}>
+                <form onSubmit={(e) => { e.preventDefault(); submit() }}>
                     <h3>Import events from another calendar using an iCal Link</h3>
                     <input type="text" ref={link} placeholder="iCal link" />
                     <button type="submit">Submit</button>
                 </form>
+                <div className="links-holder">
+                    {links.map((value, index) => <p onClick={async () => {
+                        console.log(index)
+                        let newArr = [...links]
+                        newArr.splice(index, 1)
+                        await setLinks(newArr)
+                        await updateDoc(doc(db, 'users', user), { iCalLinks: newArr })
+                    }}>{value}</p>)}
+                </div>
             </div>
-        </>
+        </div>
 
 
     )
